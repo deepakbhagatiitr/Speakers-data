6521a1b4ef33beaaff0cd7e202f46c534a835365
Deepak Bhagat <deepak988088@gmail.com>
2025-02-14 16:06:56 +0530

    Prepare for deployment

--- events/settings.py
+++ events/settings.py
@@ -83,7 +83,8 @@ DATABASES = {
     }
 }
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
